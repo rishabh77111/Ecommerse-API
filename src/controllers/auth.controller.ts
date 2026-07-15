a9ef638576a75bc90975857303f6cde1641f9dad
Rishabh <rishabhkumaragrahari77@gmail.com>
2026-07-15 10:53:58 +0545

    user model and register api

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,24 +1,76 @@
-//! register
+
 
 import { NextFunction, Request, Response } from "express";
-
-export const register=async (req:Request,res:Response,next:NextFunction)=>{
-try {
-    
-} catch (error) {
-    next(error);
-}
-}
+import User from "../models/user.model";
+import { hashPassword } from "../utils/bcrypt.utils";
 
 //! login
-
-export const login=async (req:Request,res:Response,next:NextFunction)=>{
-try {
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
     
-} catch (error) {
+  } catch (error) {
     next(error);
-}
-}
+  }
+};
+
+//! register
+
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const {full_name,email,password}=req.body;
+    if(!full_name){
+      const error:any=new Error("full_name is required");
+      error.status="fail";
+      error.statusCode=400;
+      throw error;
+      
+    }
+
+    if(!email){
+      const error:any=new Error("email is required");
+      error.status="fail";
+      error.statusCode=400;
+      throw error;
+      
+    }
+
+    if(!password){
+      const error:any=new Error("password is required");
+      error.status="fail";
+      error.statusCode=400;
+      throw error;
+      
+    }
+
+    const user=new User({email,full_name,password});
+
+    //* hash password
+    const hash=await hashPassword(password);
+    user.password=hash;
+
+    //* upload profile image
+    //* save user
+     await user.save();
+    //* success response
+    res.status(201).json({
+      message:"Account created",
+      data:user,
+      status:"success",
+      success:true,
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
 
 //! get profile
 
--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 export const errorHandler=(error:any,req:Request,res:Response,next:NextFunction)=>{
   //console.log(error);
   const message=error?.message ?? "something went wrong";
-  const status=error?.message ?? "error";
+  const status=error?.status ?? "error";
   const statusCode=error?.statusCode ??500;
 
    res.status(statusCode).json({
